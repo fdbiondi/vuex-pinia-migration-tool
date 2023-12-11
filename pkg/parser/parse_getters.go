be1d6e08eb90bff2c935c096684f77eaa1236044
fbiondi <federico.biondi@rms.com>
2023-12-11 12:19:21 -0300

    formatting

--- pkg/parser/parse_getters.go
+++ pkg/parser/parse_getters.go
@@ -14,6 +14,7 @@ var getterPattern = map[string]*regexp.Regexp{
 	string("function_lines_end"): regexp.MustCompile(`\s{2}\},$`),
 	string("getter_call"):        regexp.MustCompile(`getters\.(\w*)`),
 	string("root_state_call"):    regexp.MustCompile(`rootState\.(\w*)\.(\w*)`),
+	string("root_getter_call"):   regexp.MustCompile(`rootGetters\[("|')((\w+)\/(\w+))("|')\]`),
 }
 
 func parseGetters(filesMap map[string]*os.File) []string {
@@ -51,10 +52,11 @@ func parseGetters(filesMap map[string]*os.File) []string {
 		}
 
 		if match := getterPattern["root_state_call"].FindStringSubmatch(line); match != nil {
-
+			// get store name and function name to create instance
 			storeName := fmt.Sprintf("%s%sStore", match[1], capitalizeByteSlice(match[2]))
 			storeFn := fmt.Sprintf("use%s", capitalizeByteSlice(storeName))
 
+			// replace original line
 			var lineReplace = fmt.Sprintf("%s.$$state", storeName)
 
 			line = getterPattern["root_state_call"].ReplaceAllString(line, lineReplace)
@@ -77,7 +79,34 @@ func parseGetters(filesMap map[string]*os.File) []string {
 				lines = append([]string{importLine}, lines...)
 				importedStores = append(importedStores, storeName)
 			}
+		}
 
+		if match := getterPattern["root_getter_call"].FindStringSubmatch(line); match != nil {
+
+			storeFn := fmt.Sprintf("use%sStore", capitalizeByteSlice(match[3]))
+			storeName := fmt.Sprint(match[3], "Store")
+			fnName := match[4]
+			// args := strings.Replace(match[3], ", { root: true }", "", 1)
+
+			line = getterPattern["root_getter_call"].ReplaceAllString(line, fmt.Sprintf("%s.%s", storeName, fnName))
+
+			if !slices.Contains(intantiatedStores, storeName) {
+				// get instance of root store
+				defLine := fmt.Sprintf("\t\tconst %s = %s()", storeName, storeFn)
+
+				functionLines = append([]string{defLine}, functionLines...)
+
+				intantiatedStores = append(intantiatedStores, storeName)
+			}
+
+			if !slices.Contains(importedStores, storeName) {
+				// create import statement of store
+				importLine := fmt.Sprintf("import %s from '%s'", storeFn, fmt.Sprint("~/store/", storeName, ".ts"))
+
+				// add import statement to first line
+				lines = append([]string{importLine}, lines...)
+				importedStores = append(importedStores, storeName)
+			}
 		}
 
 		if match := getterPattern["function_lines_end"].FindStringSubmatch(line); match != nil {
